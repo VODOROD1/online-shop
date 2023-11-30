1ba638f04943fb3efb5fd8794dd15aaf860eb3ab
mikhail.belyakov <mikhail.belyakov@corp.codeinside.ru>
2023-11-30 12:06:24 +0400

    [november_30] form object was added in dialog-box-component.ts

--- src/app/components/UI/dialog-box/dialog-box.component.ts
+++ src/app/components/UI/dialog-box/dialog-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-dialog-box",
@@ -7,6 +8,10 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class DialogBoxComponent implements OnInit {
+
+  myForm: FormGroup = new FormGroup({
+    title: new FormControl('')
+  });
   
   constructor() { 
 
